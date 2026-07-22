4defb580d52eb07b33fba4d2da9947b84d3e00ad
DevKadiya7 <kadiyadev07@gmail.com>
2026-07-22 16:46:34 +0530

    feat(frontend): add dashboard API client

---
+++ frontend/src/api/dashboard.js
@@ -0,0 +1,23 @@
+export const getDashboardSummary = async () => {
+  throw new Error("Dashboard API not implemented");
+};
+
+export const getRecentPurchases = async () => {
+  throw new Error("Dashboard API not implemented");
+};
+
+export const getTopSelling = async () => {
+  throw new Error("Dashboard API not implemented");
+};
+
+export const getLowStock = async () => {
+  throw new Error("Dashboard API not implemented");
+};
+
+export const getSalesByCategory = async () => {
+  throw new Error("Dashboard API not implemented");
+};
+
+export const getMonthlySales = async () => {
+  throw new Error("Dashboard API not implemented");
+};
